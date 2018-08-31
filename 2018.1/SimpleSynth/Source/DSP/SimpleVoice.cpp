238a619d47e383f7efad5d00e3e730d2652c9d71
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2018-08-31 10:00:42 +0900

    Add comment.

--- 2018.1/SimpleSynth/Source/DSP/SimpleVoice.cpp
+++ 2018.1/SimpleSynth/Source/DSP/SimpleVoice.cpp
@@ -8,34 +8,41 @@
   ==============================================================================
 */
 
+// ヘッダファイルをインクルードする。
 #include "SimpleVoice.h"
 
+// ①無名名前空間に定数宣言を記述する。
 namespace {
 	const float HALF_PI = MathConstants<float>::halfPi;
 	const float ONE_PI = MathConstants<float>::pi;
 	const float TWO_PI = MathConstants<float>::twoPi;
 }
 
+// ②引数付きコンストラクタ
 SimpleVoice::SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopePatameters* ampEnvParams, AudioParameterBool* velocitySenseParam)
 	: _oscParamsPtr(oscParams)
 	, _lfoParamsPtr(lfoParams)
 	, _ampEnvParamsPtr(ampEnvParams)
 	, _velocitySenseParamPtr(velocitySenseParam)
 	, ampEnv(ampEnvParams->Attack->get(), ampEnvParams->Decay->get(), ampEnvParams->Sustain->get(), ampEnvParams->Release->get())
-	, currentAngle(0.0f)
-	, lfoAngle(0.0f)
-	, lastLevel(0.0f)
+	, currentAngle(0.0f), lfoAngle(0.0f), angleDelta(0.0f)
+	, level(0.0f), lastLevel(0.0f), levelDiff(0.0f)
 	, pitchBend(0.0f)
 {}
 
+// デストラクタ
 SimpleVoice::~SimpleVoice()
 {}
 
+// ③当ボイスクラスに対応するサウンドクラスが登録されているかどうかを判定する関数。
+//   Synthesiserクラスから呼び出されるインターフェース。
 bool SimpleVoice::canPlaySound(SynthesiserSound* sound)
 {
 	return dynamic_cast<const SimpleSound*> (sound) != nullptr;
 }
 
+// ④Synthesiserクラスから呼び出されるノートONのトリガー。
+//   引数で渡されたノート番号、ベロシティなどの値に対応して波形を生成する前準備を行う。
 void SimpleVoice::startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int currentPitchWheelPosition)
 {
 	DBG("startNote");
@@ -53,126 +60,127 @@ void SimpleVoice::startNote(int midiNoteNumber, float velocity, SynthesiserSound
 			lfoAngle = 0.0f;
 		}
 
-		if (_velocitySenseParamPtr->get()) {
-			
+		if (_velocitySenseParamPtr->get()) 
+		{
 			if (velocity <= 0.01f) {
 				velocity = 0.01f;
 			}
-
 			level = velocity * 0.4f; // * 0.4をしておかないと、加算合成されるので破綻する;
 		}
 		else 
 		{
 			level = 0.8f;
 		}
+		levelDiff = lastLevel - level;
 
 		pitchBend = ((float)currentPitchWheelPosition - 8192.0f) / 8192.0f;
 
+		// 生成する波形のピッチを再現するサンプルデータ間の角度差⊿θ[rad]の値を決定する。
 		float cyclesPerSecond = (float)MidiMessage::getMidiNoteInHertz(midiNoteNumber);
 		float cyclesPerSample = (float)cyclesPerSecond / (float)getSampleRate();
-
-		// startNoteではじめてangelDeltaが確定する
 		angleDelta = cyclesPerSample * TWO_PI;
 
-		// この時にサンプルレートは確定している
 		ampEnv.attackStart();
-		//ampEnv.cycle((float)getSampleRate());
-
-		levelDiff = lastLevel - level;
-
 	}
 }
 
+// ⑤Synthesiserクラスから呼び出されるノートOFFのトリガー。
+//   引数で渡される値に対応して波形生成を終了する前準備を行う。
+//   キーリリースだとallowTailOff == true、キーリリース直後のボイススチールではallowTailOff == false
 void SimpleVoice::stopNote(float velocity, bool allowTailOff)
 {
 	DBG("stopNote : " + juce::String((int)allowTailOff));
 
-	// シンセサイザークラスから呼ばれるとき、キーリリースだとallowTailOff == true、キーリリース直後のボイススチールではallowTailOff == false
 	lastLevel = level;
 
 	if (allowTailOff)
 	{
 		ampEnv.releaseStart();
-
 	}
 	else
 	{
 		// キーホールド中(ADSのいずれか)であればangleDeltaをリリース状態に移行
-		// ボイススチールを受けて直ぐに音量を0にしてしまうと、急峻な変化となりノイズの発生を引き起こすため、それを予防する処理。
-		if (ampEnv.isHolding())
-		{
+		if (ampEnv.isHolding()) {
+			// ボイススチールを受けて直ぐに音量を0にしてしまうと、急峻な変化となりノイズの発生を引き起こすため、それを予防する処理。
 			ampEnv.releaseStart();
 		}
-		// リリース状態に入っていたときはangleDeltaの値をリセット
-		else if(ampEnv.isReleasing())
-		{
+		else if(ampEnv.isReleasing()) {
+			// リリース状態に入っていたときはangleDeltaの値をリセット
 			angleDelta = 0.0f;		
 		}
-
 		// ボイススチール処理の過程で現在のノート再生状態をクリアする
 		clearCurrentNote();
 	}
 }
 
+// ⑥ピッチホイール操作をトリガーとしてSynthesiserクラスから呼び出される。ピッチベンドの値を保持する変数を更新する。
 void SimpleVoice::pitchWheelMoved(int newPitchWheelValue)
 {
-	// シンセサイザークラスから呼ばれるとき、キーリリースだとallowTailOff == true、キーリリース直後のボイススチールではallowTailOff == false
 	pitchBend = ((float)newPitchWheelValue - 8192.0f) / 8192.0f;
-
 }
 
+// ⑦MIDI CC受信をトリガーとしてSynthesiserクラスから呼び出される関数。
 void SimpleVoice::controllerMoved(int controllerNumber, int newControllerValue)
-{
-}
+{}
 
+// ⑧オーディオバッファに波形をレンダリングする関数。Synthesiserクラスから呼び出される
 void SimpleVoice::renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples)
 {
-	ampEnv.setParameters(_ampEnvParamsPtr->Attack->get(), _ampEnvParamsPtr->Decay->get(), _ampEnvParamsPtr->Sustain->get(), _ampEnvParamsPtr->Release->get());
 
-	// clearCurrentNote()実行後ではここは通れない
+
 	if (SimpleSound* playingSound = static_cast<SimpleSound*>(getCurrentlyPlayingSound().get()))
 	{
 		if (angleDelta != 0.0f)
 		{
-			while (--numSamples >= 0) // 通常のキーON時はここを通る
+			while (--numSamples >= 0)
 			{
-				levelDiff *= 0.99f;
-
-				// Modulation factor
+				// LFO:モジュレーション波形のサンプルデータを生成する。
 				float modulationFactor = calcModulationFactor(lfoAngle);
 
-				// OSC MIX
+				// OSC MIX: 
 				float currentSample = 0.0f;
 				{
 					currentSample += waveForms.sine(currentAngle) * _oscParamsPtr->SineWaveLevel->get();
-
 					currentSample += waveForms.saw(currentAngle) * _oscParamsPtr->SawWaveLevel->get();
-
 					currentSample += waveForms.triangle(currentAngle) * _oscParamsPtr->TriWaveLevel->get();
-
 					currentSample += waveForms.square(currentAngle) * _oscParamsPtr->SquareWaveLevel->get();
-
 					currentSample += waveForms.noise() * _oscParamsPtr->NoiseLevel->get();
 				}
 
-				// LFO
-				if (_lfoParamsPtr->LfoTarget->getCurrentChoiceName() == "WaveLevel")
-				{
+				// LFO: モジュレーション対象がWaveLevelの時のモジュレーション処理
+				if (_lfoParamsPtr->LfoTarget->getCurrentChoiceName() == "WaveLevel") {
 					currentSample *= modulationFactor;
 				}
 
-				// Level and ADSR
-				{
-					currentSample *= level + levelDiff;
-					currentSample *= ampEnv.getValue();
-				}
+				// OSC MIX: 
+				levelDiff *= 0.99f;						// 前回のベロシティとの差異によるノイズ発生を防ぐ。
+				currentSample *= level + levelDiff;
 
-				//バッファに対して加算処理を行う。1VOICE内でのMIXと複数VOICE間でのMIXなので加算
-				for (int channelNum = outputBuffer.getNumChannels(); --channelNum >= 0;) 
-				{
+				// AMP EG: エンベロープの値をサンプルデータに反映する。
+				currentSample *= ampEnv.getValue();
+
+				// バッファに対して加算処理を行う。ポリフォニックでは、各ボイスの音を加算処理する必要がある。
+				for (int channelNum = outputBuffer.getNumChannels(); --channelNum >= 0;) {
 					outputBuffer.addSample(channelNum, startSample, currentSample);
 				}
 
+				// エンベロープがリリース状態の場合の処理
+				if (ampEnv.isReleasing())
+				{
+					if (ampEnv.getValue() <= 0.005f) //エンベロープの値が十分に小さければ
+					{
+						ampEnv.releaseEnd();		 // エンベロープをWait状態に移行する。
+						clearCurrentNote();			 // このボイスが生成するノート情報をクリアする。
+						angleDelta = 0.0f;			 // 変数を初期値に戻す。
+						currentAngle = 0.0f;		 // 変数を初期値に戻す。
+						break;
+					}
+				}
+
+				//============ 次のサンプルデータを生成するための準備 ============
+
+				// LFO:モジュレーション対象がWaveAngleの時のモジュレーション処理
+				// ピッチベンド:MIDIキーボードのピッチベンド入力を反映する処理
 				if (_lfoParamsPtr->LfoTarget->getCurrentChoiceName() == "WaveAngle")
 				{
 					currentAngle += angleDelta * pow(2.0f, pitchBend) * modulationFactor;
@@ -183,43 +191,32 @@ void SimpleVoice::renderNextBlock(AudioBuffer<float>& outputBuffer, int startSam
 				}
 				lfoAngle += (_lfoParamsPtr->LfoSpeed->get() / (float)getSampleRate()) * TWO_PI;
 
-				ampEnv.cycle((float)getSampleRate());
-
-				if (ampEnv.isReleasing())
-				{
-					if (ampEnv.getValue() <= 0.005f) //リリースが十分に小さければ
-					{
-						ampEnv.releaseEnd();
-
-						// ボイススチール処理の過程で現在のノート再生状態をクリアする
-						clearCurrentNote();
-						angleDelta = 0.0f;
-						currentAngle = 0.0f;
-						break;
-					}
-				}
-
-				if (abs(levelDiff) < 0.0001f)
-				{
+				if (abs(levelDiff) < 0.0001f) {
 					levelDiff = 0.0f;
 				}
 
-				if (currentAngle > TWO_PI)
-				{
+				if (currentAngle > TWO_PI) {
 					currentAngle -= TWO_PI;
 				}
 
-				if (lfoAngle > TWO_PI)
-				{
+				if (lfoAngle > TWO_PI) {
 					lfoAngle -= TWO_PI;
 				}
 
+				// AMP EG: エンベロープの各パラメータを最新の値に変更する。
+				ampEnv.setParameters(_ampEnvParamsPtr->Attack->get(), _ampEnvParamsPtr->Decay->get(), _ampEnvParamsPtr->Sustain->get(), _ampEnvParamsPtr->Release->get());
+
+				// AMP EG: エンベロープを1サンプル分進めておく。
+				ampEnv.cycle((float)getSampleRate());
+
+				// 書き込み先のオーディオバッファのサンプルインデックス値をインクリメントする。
 				++startSample;
 			}
 		}
 	}
 }
 
+// ⑨LFOのモジュレーション波形を算出する関数。
 float SimpleVoice::calcModulationFactor(float angle)
 {
 	float factor = 0.0f;
@@ -244,9 +241,7 @@ float SimpleVoice::calcModulationFactor(float angle)
 	{
 		factor = waveForms.noise();
 	}
-	factor *= _lfoParamsPtr->LfoAmount->get();
-	factor /= 2.0f;
-	factor += 0.5f;
-
+	// factorの値が0.5を中心とした0.0～1.0の値となるように調整する。
+	factor = ((factor * _lfoParamsPtr->LfoAmount->get()) / 2.0f) + 0.5f;
 	return factor;
 }
--- 2018.1/SimpleSynth/Source/DSP/SimpleVoice.h
+++ 2018.1/SimpleSynth/Source/DSP/SimpleVoice.h
@@ -10,49 +10,45 @@
 
 #pragma once
 
+// ①各種ヘッダファイルをインクルードする。
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "SimpleSound.h"
-#include "AmpEnvelope.h"
-#include "Waveforms.h"
 #include "SimpleSynthParameters.h"
+#include "Waveforms.h"
+#include "AmpEnvelope.h"
+#include "SimpleSound.h"
 
+// ②クラス名宣言。SynthesiserVoiceクラスを継承する。
 class SimpleVoice : public SynthesiserVoice
 {
 public:
-	//==============================================================================
-	/** Creates a voice. */
+	// ③引数付きコンストラクタ。
 	SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopePatameters* ampEnvParams, AudioParameterBool* velocitySenseParam);
 
-	/** Destructor. */
+	// デストラクタ
 	virtual ~SimpleVoice();
 
+	// ④基底クラスで宣言された純粋仮想関数をオーバーライド宣言する。
 	virtual bool canPlaySound(SynthesiserSound* sound) override;
-
 	virtual void startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int currentPitchWheelPosition) override;
-
 	virtual void stopNote(float velocity, bool allowTailOff) override;
-
 	virtual void pitchWheelMoved(int newPitchWheelValue) override;
-
 	virtual void controllerMoved(int controllerNumber, int newControllerValue) override;
-
-	virtual void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples);
-
+	virtual void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
 
 private:
+	// ⑤クラス内でのみ呼び出す関数をprivate領域に宣言する。
 	float calcModulationFactor(float angle);
 
-	float angleDelta;
-	float currentAngle;
-    float lfoAngle;
+	// ⑥クラス内変数を宣言する。
+	float currentAngle, lfoAngle, angleDelta;
+	float level, lastLevel, levelDiff;
 	float pitchBend;
-	float level;
-	float lastLevel;
-	float levelDiff;
 
-    Waveforms waveForms;
+	// VCOとVCAのオブジェクトを宣言する。
+	Waveforms waveForms;
 	AmpEnvelope ampEnv;
 
+	// パラメータを管理するオブジェクトのポインタ変数。
 	OscillatorParameters* _oscParamsPtr;
 	LfoParameters* _lfoParamsPtr;
 	AmpEnvelopePatameters* _ampEnvParamsPtr;
